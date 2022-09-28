1dddbe67dac70c52de1643c21f0d8a65becf80aa
berhanudagnew <berhanudagnew.github@gmail.com>
2022-09-28 05:39:59 +0300

    dice final commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
     MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: Colors.red,
         appBar: AppBar(
@@ -15,9 +17,50 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  const DicePage({Key key}) : super(key: key);
+
+  @override
+  State<DicePage> createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  // dicleare a variable
+  int left_dice_number = 1;
+  int right_dice_number = 1;
+  // initialise a function
+  void change_dice_face() {
+    setState(
+      () {
+        left_dice_number = Random().nextInt(6) + 1;
+        right_dice_number = Random().nextInt(6) + 1;
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            child: TextButton(
+              onPressed: () {
+                change_dice_face();
+              },
+              child: Image.asset('images/dice$left_dice_number.png'),
+            ),
+          ),
+          Expanded(
+            child: TextButton(
+              onPressed: () {
+                change_dice_face();
+              },
+              child: Image.asset('images/dice$right_dice_number.png'),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
